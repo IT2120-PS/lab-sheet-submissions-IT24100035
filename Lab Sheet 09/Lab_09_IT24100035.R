43bc7d6bac27bb4e8ad22a5c8688ced3f2f53342
Nethmi Kariyawasam <it24100035@my.sliit.lk>
2025-10-01 20:36:54 +0530

    Lab 09

---
+++ Lab Sheet 09/Lab_09_IT24100035.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\User\\OneDrive - Sri Lanka Institute of Information Technology\\Desktop\\IT24100035")
+##Exercise
+#1).
+bake <- rnorm(25, mean = 45, sd = 2)
+bake
+
+#2).
+t.test(bake, mu = 46, alternative = "less")
+
